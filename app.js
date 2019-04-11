8215d8aaa916062292d92b27adc735adab6fb0db
Дорофеев Александр Николаевич <a.dorofeev@informika.ru>
2019-04-11 20:18:40 +0300

    write file

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    console.log(req)
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>First Page</title></head>');
-    res.write('<body><h1>Hello Node.js</h1></body>');
-    res.write('</html>');
-    res.end();
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>First Page</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "fish");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>First Page</title></head>");
+  res.write("<body><h1>Hello Node.js</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
-server.listen(3005, "localhost", () => console.log(`server running on port 3005`));
+server.listen(3005, "localhost", () =>
+  console.log(`server running on port 3005`)
+);
