be35ab0a58e825750e01f617b67441ccb201aa87
Дорофеев Александр Николаевич <a.dorofeev@informika.ru>
2019-04-11 20:51:32 +0300

    add file

--- app.js
+++ app.js
@@ -9,16 +9,25 @@ const server = http.createServer((req, res) => {
     res.write("<html>");
     res.write("<head><title>First Page</title></head>");
     res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>'
+      `<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>`
     );
     res.write("</html>");
     return res.end();
   }
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "fish");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
+    const body = [];
+    req.on("data", chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
   }
 
   res.setHeader("Content-Type", "text/html");
