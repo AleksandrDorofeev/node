638a6abfc64cd895fd32875bb379cf3792baf254
Aleksandr <adorofeev@team.amocrm.com>
2019-04-15 00:47:39 +0300

    express

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const http = require("http");
+
+const express = require("express");
+
 const routesHandler = require('./routes');
 
-const server = http.createServer((req, res) => {
-    routesHandler.routesHandler(req, res);
+const app = express();
+
+app.use((request, response, next) => {
+  console.log("Middleware!");
+  next();
 });
+
+app.use((request, response, next) => {
+  console.log("next Middleware!")
+});
+
+
+const server = http.createServer(app);
+
+// const server = http.createServer((req, res) => {
+//     routesHandler.routesHandler(req, res);
+// });
 
 server.listen(3006, "localhost", () =>
   console.log(`server running on port 3006`)
